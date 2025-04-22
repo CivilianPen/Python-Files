6e36f8d1ea64effc6fb8af79c43973846b6343fa
CivilianPen <civilianpen@yandex.ru>
2025-04-22 14:02:41 +0300

    div

--- pyton/other/sum_of_floats.py
+++ pyton/other/sum_of_floats.py
@@ -5,14 +5,12 @@ def choose_operation():
     if the_type == '+':
         get_sum()
     elif the_type == '-':
-        print('coming soon')
         get_diff()
         choose_operation()
     elif the_type == '*':
         get_mult()
         choose_operation()
     elif the_type == '/':
-        print('coming soon')
         get_divi()
         choose_operation()
     else:
@@ -90,10 +88,12 @@ def mult(a,b):
     if dot_b == -1:
         b += '.0'
         dot_b = b.find('.')
+
     dec = str(int(a[:dot_a]) * int(b[:dot_b]))
     mult_float = str(int(a[dot_a + 1:]) * int(b[dot_b + 1:]))
-    zeros = len(str(int(a[dot_a + 1:]))) + len(str(int(b[dot_b + 1:]))) - len(mult_float)
+    zeros = len(str((a[dot_a + 1:]))) + len(str((b[dot_b + 1:]))) - len(mult_float)
     mult_float = zeros * '0' + mult_float
+
     x = int(a[:dot_a])
     y = int(b[:dot_b])
     x_f = float('0.' + (a[dot_a + 1:]))
@@ -106,12 +106,44 @@ def mult(a,b):
         suma1 = suma(suma1, x_all[i])
     for i in range(0, len(y_all)):
         suma2 = suma(suma2, y_all[i])
-    formula = suma(suma(dec + '.0', '0.' + mult_float), suma(suma1, suma2))
+
+    formula = suma( f'{dec}.{mult_float}' , suma(suma1,suma2))
     return formula
+
 def diff(a, b):
-    return round(a-b , 5)
+    a = str(a)
+    b = str(b)
+    c = a
+    a = str(max(float(a),float(b)))
+    b = str(min(float(c),float(b)))
+    dot_a = a.find('.')
+    dot_b = b.find('.')
+    if dot_a == -1:
+        a += '.0'
+        dot_a = a.find('.')
+    if dot_b == -1:
+        b += '.0'
+        dot_b = b.find('.')
+
+    zeros = (max(len(a[dot_a + 1:]), len(b[dot_b + 1:]))) * '0'
+    if len(a[dot_a:]) < len(b[dot_b:]):
+        count = len(b[dot_b:]) - len(a[dot_a:])
+        a += '0' * count
+    else:
+        count = len(a[dot_a:]) - len(b[dot_b:])
+        b += '0' * count
+    F = (max(len(a[dot_a + 1:]), len(b[dot_b + 1:])))
+    dec = str(int(a[:dot_a]) - int(b[:dot_b]))
+    suma_float = str(int(a[dot_a + 1:]) - int(b[dot_b + 1:]))
+    zeros = zeros[:-len(suma_float)] + suma_float
+    if zeros[:-len(suma_float)] != '' or float(suma_float) < int('1' + '0' * F):
+        return (f'{dec}.{zeros}')
+    else:
+        return (f'{int(dec) - int(zeros[0])}.{zeros[1:]}')
+
 def divi(a, b):
     return round(a/b , 5)
+
 
 if __name__ == '__main__':
     choose_operation()
